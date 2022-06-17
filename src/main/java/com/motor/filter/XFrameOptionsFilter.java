03c4c0b46ec9f0e14f80077d24f3040fb5e9b5e6
baolequoc <100566557+baolequoc@users.noreply.github.com>
2022-06-17 23:36:53 +0700

    add filter fix antijacting

---
+++ src/main/java/com/motor/filter/XFrameOptionsFilter.java
@@ -0,0 +1,33 @@
+package com.motor.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebFilter("/*")
+public class XFrameOptionsFilter implements Filter{
+
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain)
+            throws IOException, ServletException {
+        // TODO Auto-generated method stub
+
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.addHeader("X-Frame-Options", "DENY");
+
+        filterChain.doFilter(servletRequest, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
